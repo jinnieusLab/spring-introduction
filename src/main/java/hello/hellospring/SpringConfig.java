cfb254515640c6773d6254bb393ffc52fff8a473
jinnieusLab <kitty14904@naver.com>
2024-04-22 23:30:23 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,27 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    //@Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private DataSource dataSource;
 //
@@ -27,14 +32,14 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository()); //command + P 하면 생성자 괄호 사이에 뭘 적어줘야할지 뜬다.
+        return new MemberService(memberRepository); //command + P 하면 생성자 괄호 사이에 뭘 적어줘야할지 뜬다.
     }
 
-    @Bean
-    public MemberRepository memberRepository() { // 인터페이스
+//    @Bean
+//    public MemberRepository memberRepository() { // 인터페이스
 //        return new MemoryMemberRepository(); // 구현체인 메모리멤버리포지토리를 넣어줘야함.
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
